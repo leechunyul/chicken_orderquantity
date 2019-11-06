4b8d002f9bef0ab8b4526757e653b47a7f5a52b4
fwani <fwani@gmail.com>
2019-11-06 11:14:32 +0900

    .

--- src/kosta/mvc/model/dao/ChickenDao.java
+++ src/kosta/mvc/model/dao/ChickenDao.java
@@ -1,4 +1,21 @@
 package kosta.mvc.model.dao;
 
 public interface ChickenDao {
+
+    /**
+     * 1. 지역별
+     */
+
+    /**
+     * 2. 연령대별
+     */
+
+
+    /**
+     * 3. 성별
+     */
+
+    /**
+     * 4. 시간대별
+     */
 }
---
+++ src/kosta/mvc/model/view/MenuView.java
@@ -0,0 +1,4 @@
+package kosta.mvc.model.view;
+
+public class MenuView {
+}
